a3ac444e066d1d9afbc4bb35f74ceb5285b09cc9
allison.ferreira <allison.ferreira@bandtec.com.br>
2022-05-14 01:24:03 -0300

    Adicionando nova mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Projeto criado por Allison no LAB Devops da Gaby"
+    return "Projeto criado por Allison Ferreira no LAB Devops da Gaby"
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Projeto criado por Allison no LAB Devops da Gaby")
+        self.assertEqual(self.result.data.decode('utf-8'), "Projeto criado por Allison Ferreira no LAB Devops da Gaby")
